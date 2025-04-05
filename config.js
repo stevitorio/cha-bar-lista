91c33e63b8a634adbf2a33b4f96f01d9538fce20
stevitorio <85889916+stevitorio@users.noreply.github.com>
2025-04-05 20:11:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 // Cole sua config do Firebase aqui:
-const firebaseConfig = {
-  // ...
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyCLtwSylor6Ev01hgVfMPvoiqqK-vE8zjg",
+    authDomain: "chabarstenan.firebaseapp.com",
+    projectId: "chabarstenan",
+    storageBucket: "chabarstenan.firebasestorage.app",
+    messagingSenderId: "838455574769",
+    appId: "1:838455574769:web:ed65b77807fe00bc78e950",
+    measurementId: "G-024BSCN5CN"
+  };
 firebase.initializeApp(firebaseConfig);
